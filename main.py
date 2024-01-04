785ab846d393f3b7b7702f00369c5cd5e2dee8e3
Imeguras <imeguras@cock.li>
2024-01-04 17:51:53 +0000

    Pilot

---
+++ main.py
@@ -0,0 +1,41 @@
+#import json
+#import numpy as np
+#import azure.functions as func
+#from filterpy.kalman import UnscentedKalmanFilter
+#from filterpy.common import Q_discrete_white_noise
+#from filterpy.kalman import JulierSigmaPoints
+#
+#from numpy.random import randn
+#
+## Function to predict the next state
+#def fx(x, dt):
+#    # x[0] is distance, x[1] is average speed
+#    distance = x[0] + x[1] * dt
+#    speed = x[1]
+#    return np.array([distance, speed])
+#
+## Function to update the measurement based on the state
+#def hx(x):
+#    # Assuming the measurement is the distance
+#    return np.array([x[0]])
+#
+#def main(req: func.HttpRequest) -> func.HttpResponse:
+#  try:
+#    return "Hello World!"
+#    # Create an Unscented Kalman Filter
+#    #dim_x = 2  # Dimension of the state (distance and speed)
+#    #dim_z = 1  # Dimension of the measurement (distance)
+#    #req_body = req.get_json()
+#    #sigmas = JulierSigmaPoints(n=2, kappa=3)
+#    #initial_state = np.array([0.0, 0.0])  # Initial distance and speed
+#    #initial_covariance = np.eye(dim_x) * 1.0  # Initial covariance matrix
+#    #unscentedF = UnscentedKalmanFilter(dim_x=dim_x, dim_z=dim_z, dt=1., hx=hx, fx=fx, points=sigmas )
+#    #unscentedF.P *= 5.
+#    #unscentedF.R *= 2.
+#    #unscentedF.Q = Q_discrete_white_noise(dim=2, dt=1., var=1.)
+#    #unscentedF.R = np.array([[0.1]])  # Measurement noise covariance
+#    
+#
+#  except Exception as e:
+#    return func.HttpResponse(f"Error: {str(e)}", status_code=500)
+#
